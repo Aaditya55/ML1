bcbb648afa6fb8c3d5679ea2f96d34bb93cae6af
Aaditya55 <64467870+Aaditya55@users.noreply.github.com>
2020-05-30 22:04:29 +0530

    Create mailgo.py

---
+++ mailgo.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+
+
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
+import os.path
+
+email = 'Sender email'
+password = 'Sender email password'
+sendto_email = 'asj29102000@gmail.com'
+subject = 'Model Accuracy'
+message = "hello there , you created a greate work buddy , Congratulations and visit your model , because now it gives proper accuracy" 
+msg = MIMEMultipart()
+msg['From'] = email
+msg['To'] = sendto_email
+msg['Subject'] = subject
+
